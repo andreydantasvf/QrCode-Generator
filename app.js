732c66dd3a340395f806dcf9da9e3c9460a9184d
Andrey Dantas <andreydantasvf@gmail.com>
2022-01-17 23:30:43 -0300

    QRCode Generator

---
+++ app.js
@@ -0,0 +1,6 @@
+function gerarQRCode() {
+    var inputLink = document.querySelector('textarea').value
+    var GoogleChartAPI = 'https://chart.googleapis.com/chart?cht=qr&chs=500x500&chl='
+    var conteudoQRCode = GoogleChartAPI + encodeURIComponent(inputLink)
+    document.querySelector('#qrcode-image').src = conteudoQRCode
+}
